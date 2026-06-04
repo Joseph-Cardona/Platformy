3518c424c017ea1bbf19e3051ef83dfb2bd241ba
Joseph Cardona <josephmcardona@gmail.com>
2026-06-03 20:06:42 -0700

    Attempting to add a Kaplay canvas

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import kaplay from 'kaplay';
 
 function Editor () {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    if (kaplay.isInitialized && kaplay.isInitialized()) {
+      return;
+    }
+
+    const game = kaplay({
+      width: 300,
+      height: 300,
+      background: [0, 0, 0],
+      scale: 2,
+      canvas: canvasRef.current,
+      global: false,
+    });
+
+    game.add([
+      game.text("Editor"),
+      game.pos(30, 30),
+    ]);
+
+    return () => {
+      game.quit();
+    }
+  }, []);
   return (
     <div>
       <h1>Editor</h1>
-      <p>Insert Level Editor Here</p>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 }
